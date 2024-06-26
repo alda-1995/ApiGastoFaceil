57b32d0fc85832ed950abe754ccba0febcf1ad5e
alda-1995 <aldair@canalla.agency>
2024-06-26 15:24:20 -0600

    se agregan tablas

---
+++ database/migrations/2024_06_26_192155_create_transactions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->increments('transaction_id');
+            $table->decimal('amount', 10, 2);
+            $table->mediumText('description');
+            $table->date("currentDate");
+            $table->boolean('income');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
